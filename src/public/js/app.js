cfcc707397d4c07cb20e77c842d700c495dcb21a
Tom Kim <babooteng2@gmail.com>
2021-09-08 01:46:04 +0900

    #1.5~1.7 Nicknames part One

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,18 +1,40 @@
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("#message");
+const nickForm = document.querySelector("#nick");
 const socket = new WebSocket(`ws://${window.location.host}`);
+let nick = "";
 
 socket.addEventListener("open", () => {
   console.log("Connected to the Server ✔");
 });
 
-socket.addEventListener("message", (e) => {
-  console.log(e);
-  console.log(`${e.data} from the Server`);
+socket.addEventListener("message", (message) => {
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 socket.addEventListener("close", () => {
   console.log("Closed to Server ❌");
 });
 
-setTimeout(() => {
-  socket.send("hello from the Browser");
-}, 10000);
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(makeMessage("message", input.value));
+  input.value = "";
+};
+
+const handleNickSubmit = (e) => {
+  e.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+};
+
+const makeMessage = (type, payload) => {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+};
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import http from "http";
 import WebSocket from "ws";
 import express from "express";
 
+//kill -9 $(lsof -t -i tcp:3000) - 사용포트 서버 죽이기
+
 const app = express();
 
 app.set("view engine", "pug");
@@ -14,13 +16,15 @@ const handleListen = () => console.log("Listening on http://localhost:3000");
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
-
+const sockets = [];
 wss.on("connection", (socket) => {
+  sockets.push(socket);
   console.log("Connected to the Browser ✔");
   socket.on("close", () => console.log("Disconnected from the Browser ❌"));
-
-  socket.send("hello!");
-  socket.on("message", (message) => console.log(message.toString("utf-8")));
+  socket.on("message", (message) => {
+    //console.log(message.toString("utf-8"));
+    sockets.forEach((aSocket) => aSocket.send(message));
+  });
 });
 
 server.listen(3000, handleListen);
