70588bcca68156080ef25542aa56843df98c6fcc
Tom Kim <babooteng2@gmail.com>
2021-09-12 00:58:49 +0900

    #2.8 Room Count Part One

--- src/server.js
+++ src/server.js
@@ -17,12 +17,40 @@ const handleListen = () => console.log("Listening on http://localhost:3000");
 const server = http.createServer(app);
 const wsServer = SocketIO(server);
 
+function publicRooms() {
+  const {
+    sockets: {
+      adapter: { sids, rooms },
+    },
+  } = wsServer;
+  const publicRooms = [];
+  rooms.forEach((_, key) => {
+    if (sids.get(key) === undefined) {
+      publicRooms.push(key);
+    }
+  });
+  return publicRooms;
+  /* sids 는 자동적으로 private room을 하나 가짐. 그래서 rooms의 key로 sids를 조회해서 없는 방은 public 방임
+  rooms: Map(4) {
+    '6zs9kAhX-4Upa1kKAAAF' => Set(1) { '6zs9kAhX-4Upa1kKAAAF' },
+    'aeuHm91sdNI6lBZXAAAH' => Set(1) { 'aeuHm91sdNI6lBZXAAAH' },
+    '4' => Set(1) { '6zs9kAhX-4Upa1kKAAAF' },
+    'k' => Set(1) { 'aeuHm91sdNI6lBZXAAAH' }
+  },
+  sids: Map(2) {
+    '6zs9kAhX-4Upa1kKAAAF' => Set(2) { '6zs9kAhX-4Upa1kKAAAF', '4' },
+    'aeuHm91sdNI6lBZXAAAH' => Set(2) { 'aeuHm91sdNI6lBZXAAAH', 'k' }
+  },
+ */
+}
+
 wsServer.on("connection", (socket) => {
   //console.log(socket.id)
   // kind of middle ware
   socket["nickname"] = "Anonymouse";
 
   socket.onAny((event) => {
+    //console.log(wsServer.sockets.adapter);
     console.log(`Socket Event : ${event}`);
   });
 
