e145c9367004447558113c92d3cbeb94baac2089
Tom Kim <babooteng2@gmail.com>
2021-09-28 00:27:00 +0900

    #3.7 IceCandidate

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -5,6 +5,7 @@ const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
 const camerasSelect = document.getElementById("cameras");
 const call = document.getElementById("call");
+const peerFace = document.getElementById("peerFace");
 
 let myStream;
 let muted = false;
@@ -81,14 +82,17 @@ function handleCameraClick() {
 async function handleCameraChange() {
   const deviceId = camerasSelect.value;
   await getMedia(deviceId);
+  if (myPeerConnection) {
+    const videoTrack = myStream.getVideoTracks()[0];
+    const videoSender = myPeerConnection
+      .getSenders()
+      .find((sender) => sender.track.kind == "video");
+    videoSender.replaceTrack(videoTrack);
+  }
 }
 
-muteBtn.addEventListener("click", handleMuteClick);
-cameraBtn.addEventListener("click", handleCameraClick);
-// camerasSelect.addEventListener("input", handleCameraChange);
-camerasSelect.addEventListener("click", handleCameraChange);
-
 // Welcome Form ( join a room )
+
 const welcome = document.getElementById("welcome");
 welcomeForm = welcome.querySelector("form");
 
@@ -109,17 +113,38 @@ async function handleWelcomeSubmit(e) {
 }
 
 // RTC Code
+
 function makeConnection() {
   myPeerConnection = new RTCPeerConnection();
+  myPeerConnection.addEventListener("icecandidate", handleIce);
+  myPeerConnection.addEventListener("track", handleAddStream);
   myStream
     .getTracks()
     .forEach((track) => myPeerConnection.addTrack(track, myStream));
 }
 
+function handleIce(data) {
+  console.log("sent candidate");
+  socket.emit("ice", data.candidate, roomName);
+}
+
+function handleAddStream(data) {
+  console.log(
+    "got an stream from my peer - peer's stream : ",
+    data.streams[0],
+    "my stream : ",
+    myStream
+  );
+  peerFace.srcObject = data.streams[0];
+}
+
 function init() {
   call.hidden = true;
+  muteBtn.addEventListener("click", handleMuteClick);
+  cameraBtn.addEventListener("click", handleCameraClick);
+  // camerasSelect.addEventListener("input", handleCameraChange);
+  camerasSelect.addEventListener("click", handleCameraChange);
   welcomeForm.addEventListener("submit", handleWelcomeSubmit);
-  // 브라우저가 모두 local, Remote description을 가지면 됨 - 그것을 위한 핑퐁
   socket.on("welcome", async () => {
     const offer = await myPeerConnection.createOffer();
     myPeerConnection.setLocalDescription(offer);
@@ -127,12 +152,20 @@ function init() {
     socket.emit("offer", offer, roomName);
   });
   socket.on("offer", async (offer) => {
+    console.log("received the offer");
     myPeerConnection.setRemoteDescription(offer);
     const answer = await myPeerConnection.createAnswer();
+    myPeerConnection.setLocalDescription(answer);
     socket.emit("answer", answer, roomName);
+    console.log("sent the answer");
   });
   socket.on("answer", (answer) => {
+    console.log("received the answer");
     myPeerConnection.setRemoteDescription(answer);
+  });
+  socket.on("ice", (ice) => {
+    console.log("receive candidate");
+    myPeerConnection.addIceCandidate(ice);
   });
 }
 
