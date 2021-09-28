ff3c3d18d65280cc1b26de4805c836fc1e91517a
Tom Kim <babooteng2@gmail.com>
2021-09-29 01:18:36 +0900

    #3.11 Data Channels

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -12,6 +12,7 @@ let muted = false;
 let cameraOff = false;
 let roomName;
 let myPeerConnection;
+let myDataChannel;
 
 async function getCameras() {
   try {
@@ -152,12 +153,23 @@ function init() {
   camerasSelect.addEventListener("click", handleCameraChange);
   welcomeForm.addEventListener("submit", handleWelcomeSubmit);
   socket.on("welcome", async () => {
+    myDataChannel = myPeerConnection.createDataChannel("chat");
+    myDataChannel.addEventListener("message", (e) => {
+      console.log(e.data);
+    });
+    console.log("made data channel");
     const offer = await myPeerConnection.createOffer();
     myPeerConnection.setLocalDescription(offer);
     console.log("sent the offer");
     socket.emit("offer", offer, roomName);
   });
   socket.on("offer", async (offer) => {
+    myPeerConnection.addEventListener("datachannel", (e) => {
+      myDataChannel = e.channel;
+      myDataChannel.addEventListener("message", (e) => {
+        console.log(e.data);
+      });
+    });
     console.log("received the offer");
     myPeerConnection.setRemoteDescription(offer);
     const answer = await myPeerConnection.createAnswer();
